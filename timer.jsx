a4275b32284ff99deab64d6e662f3007fffbab78
楼昱凡 <yufan.lou@grandhonor.net>
2015-11-02 17:27:30 +0800

    Add TimerWrapper frame

--- timer.jsx
+++ timer.jsx
@@ -1,1 +1,24 @@
 /* globals React */
+var TimerWrapper = React.createClass({
+  getInitialState: function () {
+    return {time: null, int: null};
+  },
+  startTimer: function (time) {
+  },
+  render: function () {
+    return (
+      <div>
+        <h2>Timer</h2>
+        <Button time="5" startTimer={this.startTimer}/>
+        <Button time="10" startTimer={this.startTimer}/>
+        <Button time="15" startTimer={this.startTimer}/>
+        <Timer time={this.state.time}/>
+      </div>
+    );
+  }
+});
+
+React.render(
+  <TimerWrapper/>,
+  document.body
+);
